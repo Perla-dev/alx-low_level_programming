8133a67b81bf02f2d6b60e6bd6e50e07bbf68a10
Perla-dev <yumadev7@gmail.com>
2023-07-19 16:57:39 -0700

    fix

--- 0x03-debugging/positive_or_negative.c
+++ 0x03-debugging/positive_or_negative.c
@@ -1,24 +1,23 @@
-#include <stdlib.h>
-#include <time.h>
-#include <stdio.h>
+#include "main.h"
 /**
- * main - The program generates a new random number each time it is executed,
- * while also keeping the original structure and intent of the sentence.
- * assigns a random number to int n everytime.
- *
- * Return: Always 0 (Success)
- */
-int main(void)
+  *main - Determine if anumber is positive, negative or zero.
+  (* 0 : is the number to be checked
+  * Return: 0 on success
+  */
+void positive_or_negative(int i)
 {
-	int n;
+	if (i < 0)
+	{
+		printf("%d is %s\n", i, "negative");
+	}
+	else if (i > 0)
+	{
+		printf("%d is %s\n", i, "positive");
+	}
+	else
+	{
+		printf("%d is %s\n", i, "zero");
+	}
+	return;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	if (n > 0)
-		printf("%d is positive\n", n);
-	else if (n == 0)
-		printf("%d is zero\n", n);
-	else if (n < 0)
-		printf("%d is negative\n", n);
-	return (0);
 }
